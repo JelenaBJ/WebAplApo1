44ec49e92fd404512f5a1f90d048185aaf583bac
MilaSavovic <164034831+MilaSavovic@users.noreply.github.com>
2026-01-05 21:06:34 +0100

    Update IOrderRepository.cs

--- Health/HealthHub/Models/IOrderRepository.cs
+++ Health/HealthHub/Models/IOrderRepository.cs
@@ -5,10 +5,5 @@ using System.Threading.Tasks;
 
 namespace HealthHub.Models
 {
-    public interface IOrderRepository
-    {
-        IQueryable<Order> Orders { get; }
-        void SaveOrder(Order order);
-
-    }
+   
 }
