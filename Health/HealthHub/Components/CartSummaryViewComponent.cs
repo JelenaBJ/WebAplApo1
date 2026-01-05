e1c14a147847f6968c9835bf9f770002e3f2051b
JelenaBJ <basicjelena64@gmail.com>
2026-01-05 21:43:20 +0100

    dodajem CartSummaryViewComponent.cs

--- Health/HealthHub/Components/CartSummaryViewComponent.cs
+++ Health/HealthHub/Components/CartSummaryViewComponent.cs
@@ -10,15 +10,7 @@ namespace HealthHub.Components
 {
     public class CartSummaryViewComponent : ViewComponent
     {
-        private Cart cart;
-        public CartSummaryViewComponent(Cart cartService) 
-        {
-            cart = cartService;
-        }
-        public IViewComponentResult Invoke() 
-        {
-            return View(cart);
-        }
+        
 
     }
 }
