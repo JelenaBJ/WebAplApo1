33190948a7f7cffd616d509685bd5107f6cceab7
JelenaBJ <basicjelena64@gmail.com>
2026-01-05 19:10:09 +0100

    Update HomeController.cs

--- Health/HealthHub/Controllers/HomeController.cs
+++ Health/HealthHub/Controllers/HomeController.cs
@@ -11,34 +11,14 @@ namespace HealthHub.Controllers
     public class HomeController: Controller
     {
         private I_StoreRepository repository; 
-        public int PageSize = 5; 
+        public int PageSize = 4; 
 
         public HomeController(I_StoreRepository repo)
         {
             repository = repo;
         } 
 
-        public ViewResult Index(string? category , int productPage = 1) 
-            => View(new ProductListViewModel 
-            {
-                Products = repository.Products
-                    .Where(p => category == null || p.Category == category) 
-                    .OrderBy(p => p.ProductID)
-                    .Skip((productPage - 1) * PageSize)
-                    .Take(PageSize),
-
-                PagingInfo = new PagingInfo 
-            {
-            	CurrentPage = productPage,
-            	ItemsPerPage = PageSize,
-            	TotalItems = category == null
-                	? repository.Products.Count()
-                	: repository.Products.Where(e =>
-                	e.Category == category).Count() 
-            },
-             CurrentCategory = category
- 
-            });
+      
 
  
 
