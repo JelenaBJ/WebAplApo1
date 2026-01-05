df17ca6f0ca388bd48a5fec14c962c379fabc756
MilaSavovic <164034831+MilaSavovic@users.noreply.github.com>
2026-01-05 21:10:04 +0100

    Update CartModel.cs

--- Health/HealthHub/Pages/CartModel.cs
+++ Health/HealthHub/Pages/CartModel.cs
@@ -29,23 +29,7 @@ namespace HealthHub.Pages
             //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
         
-        public IActionResult OnPost(long productId, string returnUrl) 
-        {
-            Product? product = repository.Products
-                .FirstOrDefault(p => p.ProductID == productId);
-            if (product != null) 
-            {
-                Cart.AddItem(product, 1);
-            }
-            return RedirectToPage(new { returnUrl = returnUrl });
-        }   
-        public IActionResult OnPostRemove(long productId, string returnUrl) 
-        {
-            Cart.RemoveLine(Cart.Lines.First(cl =>
-                cl.Product.ProductID == productId).Product);
-            return RedirectToPage(new { returnUrl = returnUrl });
-        }   
-
+        
 
 
 
