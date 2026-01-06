233594be8dc1d03e227984c1e7d99f68c615934c
JelenaBJ <basicjelena64@gmail.com>
2026-01-06 12:56:04 +0100

    promenjeno I_StoreRepository
    Removed product management methods from I_StoreRepository interface.

--- Health/HealthHub/Models/I_StoreRepository.cs
+++ Health/HealthHub/Models/I_StoreRepository.cs
@@ -7,11 +7,7 @@ namespace HealthHub.Models
 {
     public interface I_StoreRepository
     {
-        IQueryable<Product> Products { get; }
-
-        void SaveProduct(Product p);
-        void CreateProduct(Product p);
-        void DeleteProduct(Product p);
+       
 
     }
 }
