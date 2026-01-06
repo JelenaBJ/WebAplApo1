8a98d3ec04e82a291ebd074ee0fb6490c528add7
MilaSavovic <164034831+MilaSavovic@users.noreply.github.com>
2026-01-06 09:56:50 +0100

    Update SessionExtensions.cs

--- Health/HealthHub/Infrastructure/SessionExtensions.cs
+++ Health/HealthHub/Infrastructure/SessionExtensions.cs
@@ -13,7 +13,12 @@ namespace HealthHub.Infrastructure
             session.SetString(key, JsonSerializer.Serialize(value));
         } 
 
-        
+        public static T? GetJson<T>(this ISession session, string key) 
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null
+                ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
+        } 
 
     }
 }
