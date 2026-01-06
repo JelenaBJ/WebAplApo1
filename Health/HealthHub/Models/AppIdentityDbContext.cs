6e2950aba86c701e85f4dcc4ea535083fa236b62
JelenaBJ <basicjelena64@gmail.com>
2026-01-06 21:16:29 +0100

    konstuktor dodat Završenoo AppIdentityDbContext

--- Health/HealthHub/Models/AppIdentityDbContext.cs
+++ Health/HealthHub/Models/AppIdentityDbContext.cs
@@ -11,7 +11,9 @@ namespace HealthHub.Models
 {
     public class AppIdentityDbContext : IdentityDbContext<IdentityUser> 
     {
-        
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options) { }
     }
 
 }
+
