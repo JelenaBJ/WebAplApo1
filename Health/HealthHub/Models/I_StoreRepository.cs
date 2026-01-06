fc38a8470498dbb8e09bbc96c0d68ad2e4d4d45c
JelenaBJ <basicjelena64@gmail.com>
2026-01-06 21:12:06 +0100

    methode Gotovo I_StoreRepository

--- Health/HealthHub/Models/I_StoreRepository.cs
+++ Health/HealthHub/Models/I_StoreRepository.cs
@@ -7,7 +7,12 @@ namespace HealthHub.Models
 {
     public interface I_StoreRepository
     {
-       
+        IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
 
     }
 }
+
