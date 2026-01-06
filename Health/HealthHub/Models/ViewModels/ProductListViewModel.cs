6068ca796a1b5371ceb70cc640e9015d7bfac2ec
JelenaBJ <basicjelena64@gmail.com>
2026-01-06 21:14:06 +0100

    završeno  ProductListViewModel

--- Health/HealthHub/Models/ViewModels/ProductListViewModel.cs
+++ Health/HealthHub/Models/ViewModels/ProductListViewModel.cs
@@ -7,7 +7,10 @@ namespace HealthHub.Models.ViewModels
 {
     public class ProductListViewModel
     {
-       
+        public IEnumerable<Product> Products { get; set; } = Enumerable.Empty<Product>(); 
+        public PagingInfo PagingInfo { get; set; } = new(); 
+        public string? CurrentCategory { get; set; }
 
     }
 }
+
