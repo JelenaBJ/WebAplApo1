3c636b010f58a0ffa81deee7bbc95b23592a1d7f
ivananovic02 <ivana.novic02@gmail.com>
2026-01-06 15:31:53 +0100

    Update and rename IdentitySeedData.cs to Edit1_IdentitySeedData.cs

--- Health/HealthHub/Models/Edit1_IdentitySeedData.cs
+++ Health/HealthHub/Models/Edit1_IdentitySeedData.cs
@@ -22,21 +22,3 @@ namespace HealthHub.Models
             {
                 context.Database.Migrate();
             }
-        
-            UserManager<IdentityUser> userManager = app.ApplicationServices
-                .CreateScope().ServiceProvider
-                .GetRequiredService<UserManager<IdentityUser>>();
-
-            IdentityUser user = await userManager.FindByNameAsync(adminUser);
-            
-            if (user == null) 
-            {
-                user = new IdentityUser("Admin");
-                user.Email = "admin@example.com";
-                user.PhoneNumber = "555-1234";
-                await userManager.CreateAsync(user, adminPassword);
-            }
-        }
-
-    }
-}
